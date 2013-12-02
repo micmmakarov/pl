05daa3b3fdfb664c47114d4fed301f8a8eddb381
Michael Makarov <info1117@gmail.com>
2013-12-02 00:26:23 -0800

    Adds about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Yt::Application.routes.draw do
   devise_for :users
   get "/profile" => "users#profile"
+  get "/about" => "home#about"
   get "/uploaded" => "home#uploaded"
   get '/auth/:provider/callback' => 'authentications#create'
   post "/post" => "home#index"
